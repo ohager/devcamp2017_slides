b1371452846fbed2da5b7ec820dac82c0148a56d
ohager <oliver.hager@dextra-sw.com>
2017-06-29 23:00:07 -0300

    prod changes

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -10,7 +10,7 @@ module.exports = {
 	output: {
 		path: path.join(__dirname, "dist"),
 		filename: "bundle.js",
-		publicPath: "/dist/"
+		publicPath: "/dist/devcamp2017_slides/"
 	},
 	plugins: [
 		new webpack.optimize.OccurrenceOrderPlugin(),
